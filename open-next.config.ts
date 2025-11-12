d4058e375450ec81c53c0c88693f3acc578b9301
ardy1004 <salambumiproperty@gmail.com>
2025-11-12 19:22:12 +0700

    Update open-next.config.ts with full Cloudflare config

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,10 +1,24 @@
 export default {
   default: {
     override: {
-      wrapper: "cloudflare-edge",
+      wrapper: "cloudflare-node",
       converter: "edge",
+      proxyExternalRequest: "fetch",
       incrementalCache: "dummy",
       tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
+  edgeExternals: ["node:crypto"],
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
     },
   },
 };
