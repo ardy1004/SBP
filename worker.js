929aa9cc3f163f3b47c46aace5cb5a60bc543306
ardy1004 <salambumiproperty@gmail.com>
2025-11-13 05:27:40 +0700

    fix: Aktifkan R2 storage untuk upload gambar

--- worker.js
+++ worker.js
@@ -45,18 +45,16 @@ export default {
 		  });
 		}
 
-		// --- SIMPAN FILE ASLI KE R2 ---
+		// --- SIMPAN FILE KE R2 ---
 		const timestamp = Date.now();
-		const originalKey = `images/${propertyId}/${timestamp}-original.${ext}`;
+		const fileKey = `images/${propertyId}/${timestamp}-original.${ext}`;
+		await env.IMAGES_BUCKET.put(fileKey, file, {
+		  httpMetadata: { contentType: file.type || `image/${ext}` },
+		});
 
-		// TODO: Uncomment when R2 bucket is available
-		// await env.IMAGES_BUCKET.put(originalKey, file, {
-		//   httpMetadata: { contentType: file.type || `image/${ext}` },
-		// });
-
-		// --- RETURN URL (Konversi WebP dilakukan di backend) ---
+		// --- RETURN URL ---
 		const publicDomain = 'https://images.salambumi.xyz';
-		const imageUrl = `${publicDomain}/${originalKey}`;
+		const imageUrl = `${publicDomain}/${fileKey}`;
 
 		return new Response(JSON.stringify({
 		  success: true,
