0c8778e3636a77be9975ad50bad1683902139c8d
ardy1004 <salambumiproperty@gmail.com>
2025-11-18 08:08:54 +0700

    fix: Correct og:image meta tag format for social sharing

--- client/src/pages/PropertyDetailPage.tsx
+++ client/src/pages/PropertyDetailPage.tsx
@@ -296,7 +296,7 @@ export default function PropertyDetailPage() {
         {/* Open Graph */}
         <meta property="og:title" content={getTitle()} />
         <meta property="og:description" content={property.deskripsi || `${property.jenisProperti.charAt(0).toUpperCase() + property.jenisProperti.slice(1).replace(/_/g, ' ')} ${property.status} di ${property.kabupaten}, ${property.provinsi}. Harga: ${formatPrice(property.hargaProperti)}`} />
-        <meta property="og:image" content={`${window.location.origin}${getPrimaryImage()}`} />
+        <meta property="og:image" content={getPrimaryImage()} />
         <meta property="og:url" content={window.location.href} />
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content="Salam Bumi Property" />
@@ -305,7 +305,7 @@ export default function PropertyDetailPage() {
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={getTitle()} />
         <meta name="twitter:description" content={property.deskripsi || `Properti ${property.status} di ${property.kabupaten}`} />
-        <meta name="twitter:image" content={`${window.location.origin}${getPrimaryImage()}`} />
+        <meta name="twitter:image" content={getPrimaryImage()} />
       </Helmet>
 
       <div className="min-h-screen flex flex-col">
